c117412517e870a58d6ef807d7fb261e7460c96e
nayeem407 <sabbirahmednayeem941@gmail.com>
2022-03-10 15:46:15 +0600

    simple commit

--- index.js
+++ index.js
@@ -14,8 +14,7 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.t8ils.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-// console.log(uri)
-
+// start
 async function run() {
     // all data collection
     try{
